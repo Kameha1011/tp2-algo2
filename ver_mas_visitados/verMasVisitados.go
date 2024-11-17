6bc305e7d4da4025ee92078651424b2213cf0626
kameha1011 <omarcitofc2001@gmail.com>
2024-11-17 16:40:37 -0300

    modularizado ver_mas_visitados y ver_visitantes

--- ver_mas_visitados/verMasVisitados.go
+++ ver_mas_visitados/verMasVisitados.go
@@ -1,1 +1,42 @@
 package vermasvisitados
+
+import (
+	"fmt"
+	"strconv"
+	TDAColaPrioridad "tdas/cola_prioridad"
+	TDADiccionario "tdas/diccionario"
+	aux "tp2/auxiliares"
+)
+
+const (
+	_ARIDAD        = 1
+	_MENSAJE_ERROR = "Error en comando ver_mas_visitados"
+)
+
+type urlVisitas struct {
+	url  string
+	cant int
+}
+
+func funcionComparacion(a, b urlVisitas) int {
+	return a.cant - b.cant
+}
+
+func verMasVisitados(args []string, urls TDADiccionario.Diccionario[string, int]) {
+	if aux.VerificarArgs(args, _ARIDAD, _MENSAJE_ERROR) {
+		return
+	}
+
+	arr, cant_visitados := aux.arrADict(urls), args[0]
+	k, _ := strconv.Atoi(cant_visitados)
+	heap := TDAColaPrioridad.CrearHeapArr(arr, funcionComparacion)
+	if k > heap.Cantidad() {
+		k = heap.Cantidad()
+	}
+
+	for i := 0; i < k; i++ {
+		elemento := heap.Desencolar()
+		fmt.Printf("\t%s - %d\n", elemento.url, elemento.cant)
+	}
+	fmt.Println("OK")
+}
--- ver_visitantes/verVisitantes.go
+++ ver_visitantes/verVisitantes.go
@@ -1,9 +1,31 @@
 package vervisitantes
 
 import (
-	Diccionario "tdas/diccionario"
+	"fmt"
+	TDADiccionario "tdas/diccionario"
 	aux "tp2/auxiliares"
 )
 
-func VerVisitantes(args []string, dict Diccionario.DiccionarioOrdenado[aux.IPs, string]) {
+const (
+	_ARIDAD        = 2
+	_MENSAJE_ERROR = "Error en comando ver_visitantes"
+)
+
+func VerVisitantes(args []string, visitantes TDADiccionario.DiccionarioOrdenado[aux.IPs, string]) {
+	if aux.VerificarArgs(args, _ARIDAD, _MENSAJE_ERROR) {
+		return
+
+	}
+
+	inicio, fin := args[0], args[1]
+	comenzar := aux.parsearIp(inicio)
+	final := aux.parsearIp(fin)
+	iterRango := visitantes.IteradorRango(&comenzar, &final)
+	for iterRango.HaySiguiente() {
+		_, ip := iterRango.VerActual()
+		fmt.Printf("\t%s\n", ip)
+		iterRango.Siguiente()
+	}
+
+	fmt.Println("OK")
 }
